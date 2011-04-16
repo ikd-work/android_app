5d3764076c55548eb510ca8b27efe669b3abea9a
ikd-work <daisuke.25@hotmail.co.jp>
2011-04-17 01:36:17 +0900

    first commit

---
+++ src/com/zabbix/Login.java
@@ -0,0 +1,46 @@
+package com.zabbix;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends Activity {
+	
+	private static final int SHOW_EDITOR = 0;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_LEFT_ICON);
+        setContentView(R.layout.main);
+        getWindow().setFeatureDrawableResource( Window.FEATURE_LEFT_ICON, R.drawable.zabbix );
+        setTitle(R.string.title_login);
+        Button button = (Button)findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener(){
+        	
+        	public void onClick(View v) {
+        		Intent intent = new Intent(Login.this, SuccessActivity.class);
+        		EditText editText = (EditText)findViewById(R.id.account_name);
+        		CharSequence text = editText.getText();
+        		intent.putExtra("NAME", text);
+        		startActivityForResult(intent, SHOW_EDITOR);
+        	}
+        });
+        
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if(requestCode == SHOW_EDITOR) {
+    		if(resultCode == RESULT_OK) {
+    			TextView textView = (TextView)findViewById(R.id.account_name);
+    			textView.setText(data.getCharSequenceExtra("NAME"));
+    		}
+    	}
+    }
+}
